ceaf492b7125a001e494c296818e75d3da9b04bc
Sally Schaffner <schaffners@vpcc.edu>
2024-12-31 17:18:48 -0500

    Sum of Digits

--- M2 Examples Sp2025.cpp
+++ M2 Examples Sp2025.cpp
@@ -1,9 +1,15 @@
-// M2 Examples Sp2025.cpp : This file contains examples for M2. Arrays, Linked Lists, and Recursion
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Arrays, Linked Lists, and Recursion\n";
+// Recursive function to find the sum of digits
+int sumOfDigits(int n) {
+    if (n == 0) // Base case
+        return 0;
+    return (n % 10) + sumOfDigits(n / 10); // Recursive case
 }
 
+int main() {
+    int num = 1234;
+    cout << "Sum of digits of " << num << " is: " << sumOfDigits(num) << endl;
+    return 0;
+}
