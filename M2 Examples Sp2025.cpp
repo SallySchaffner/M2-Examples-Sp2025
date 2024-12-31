7cd2a026a7abeb07851f9c5d432a3700c379b20a
Sally Schaffner <schaffners@vpcc.edu>
2024-12-31 17:23:19 -0500

    Added iterative version of factorial function

--- M2 Examples Sp2025.cpp
+++ M2 Examples Sp2025.cpp
@@ -2,14 +2,26 @@
 using namespace std;
 
 // Recursive function for factorial
-int factorial(int n) {
+int factorialRecursive(int n) {
     if (n == 0 || n == 1) // Base case
         return 1;
-    return n * factorial(n - 1); // Recursive case
+    return n * factorialRecursive(n - 1); // Recursive case
+}
+
+// Iterative function for factorial 
+int factorialIterative(int n) {
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
 }
 
 int main() {
     int num = 5;
-    cout << "Factorial of " << num << " is: " << factorial(num) << endl;
+
+    cout << "Factorial of " << num << " (recursive): " << factorialRecursive(num) << endl;
+    cout << "Factorial of " << num << " (iteration): " << factorialIterative(num) << endl;
     return 0;
 }
+
