31719d4990448d069e4659eb2d099def27ece5f8
Sally Schaffner <schaffners@vpcc.edu>
2024-12-31 17:34:21 -0500

    Multiple Recursion

--- M2 Examples Sp2025.cpp
+++ M2 Examples Sp2025.cpp
@@ -1,9 +1,25 @@
-// M2 Examples Sp2025.cpp : This file contains examples for M2. Arrays, Linked Lists, and Recursion
+// Multiple Recursion
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Arrays, Linked Lists, and Recursion\n";
+// Recursive function to generate all permutations
+void permute(string str, int l, int r) {
+    if (l == r) { // Base case
+        cout << str << endl;
+        return;
+    }
+    for (int i = l; i <= r; i++) {
+        swap(str[l], str[i]);             // Swap characters
+        permute(str, l + 1, r);           // Recursive call
+        swap(str[l], str[i]);             // Backtrack
+    }
 }
 
+int main() {
+    string s = "ABC";
+    cout << "Permutations of " << s << ":\n";
+    permute(s, 0, s.length() - 1);
+    return 0;
+}
