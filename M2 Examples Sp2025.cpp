a876fb06fbf17df7f2958d1ae0da637c28ca24ef
Sally Schaffner <schaffners@vpcc.edu>
2024-12-31 17:26:47 -0500

    Added iterative version of Fibanocci function

--- M2 Examples Sp2025.cpp
+++ M2 Examples Sp2025.cpp
@@ -2,19 +2,37 @@
 using namespace std;
 
 // Recursive function for Fibonacci
-int fibonacci(int n) {
+int fibonacciRecursive(int n) {
     if (n == 0) // Base case
         return 0;
     if (n == 1) // Base case
         return 1;
-    return fibonacci(n - 1) + fibonacci(n - 2); // Recursive case
+    return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2); // Recursive case
 }
+
+void fibonacciIterative(int terms) {
+    int a = 0, b = 1, c;
+    cout << "Fibonacci series (iterative): " << a << " " << b << " ";
+    for (int i = 2; i < terms; i++) {
+        c = a + b;
+        cout << c << " ";
+        a = b;
+        b = c;
+    }
+    cout << endl;
+}
+
 
 int main() {
     int terms = 6;
-    cout << "Fibonacci series: ";
+    cout << "Fibonacci series (recursive): ";
     for (int i = 0; i < terms; i++) {
-        cout << fibonacci(i) << " ";
+        cout << fibonacciRecursive(i) << " ";
     }
+    cout << endl;
+
+    int n = 6;
+    fibonacciIterative(n);
+
     return 0;
 }
